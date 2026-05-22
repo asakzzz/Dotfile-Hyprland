aa4534649ae0eeee30e9867a45b3656e4980629b
Leo Girard <leo.girard@epitech.eu>
2026-05-22 23:31:51 +0200

    feat : adding zsh config

--- install.sh
+++ install.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-echo "--- Installing Packages ---"
-yay -S --needed hyprland rofi discord steam spotify noctalia-shell \
-    docker docker-compose zen-browser-bin visual-studio-code \
-    make thunar fastfetch kitty ttf-font-awesome 
-
 DOTFILES_DIR=$(pwd)
+
+echo "--- Installing Packages ---"
+yay -S  --noconfirm hyprland rofi discord steam spotify noctalia-shell \
+    docker docker-compose zen-browser-bin visual-studio-code \
+    make thunar fastfetch kitty ttf-font-awesome \
+    zsh
+
 mkdir -p "$HOME/.config"
 mkdir -p "$HOME/.local/share/icons"
+mkdir -p "$HOME/Downloads/Wallpapers"
 
-
-echo "--- Linking Configs ---"
+echo "--- Linking .config Directories ---"
 configs=("hypr" "kitty" "noctalia" "rofi" "fastfetch")
 
 for config in "${configs[@]}"; do
@@ -20,19 +22,37 @@ for config in "${configs[@]}"; do
     fi
 done
 
+echo "--- Linking Home Dotfiles ---"
+home_files=(".zshrc" ".profile" ".zprofile")
 
+for file in "${home_files[@]}"; do
+    if [ -f "$DOTFILES_DIR/zsh/$file" ]; then
+        ln -sfn "$DOTFILES_DIR/zsh/$file" "$HOME/$file"
+        echo "Linked $file to $HOME/"
+    else
+        echo "Notice: $file not found in $DOTFILES_DIR/zsh/, skipping."
+    fi
+done
+
+echo "--- Linking Assets ---"
+# Link Icons
 if [ -d "$DOTFILES_DIR/icons" ]; then
-    echo "--- Linking Cursors ---"
     ln -sfn "$DOTFILES_DIR/icons" "$HOME/.local/share/icons/my-icons"
-    ln -sfn "$DOTFILES_DIR/icons" "$HOME/.icons"
-    echo "Linked icons/cursors"
+    echo "Linked icons"
 fi
 
 if [ -d "$DOTFILES_DIR/Wallpapers" ]; then
-    echo "--- Linking Wallpapers ---"
-    ln -sfn "$DOTFILES_DIR/Wallpapers" "$HOME/Pictures/Wallpapers"
-    echo "Linked Wallpapers to ~/Pictures/Wallpapers"
+    ln -sfn "$DOTFILES_DIR/Wallpapers" "$HOME/Downloads/Wallpapers"
+    echo "Linked Wallpapers"
+fi
+
+echo "--- Setting Zsh as Default Shell ---"
+if [[ "$SHELL" != */zsh ]]; then
+    echo "Changing your default shell to zsh..."
+    chsh -s "$(which zsh)"
+else
+    echo "Zsh is already your default shell."
 fi
 
 echo "--- Setup Complete! ---"
-echo "Before rebooting , download the necessary drivers for your graphic card"
+echo "Remember to install your GPU drivers before rebooting."
