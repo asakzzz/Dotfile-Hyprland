054b1b544cefa9d93a4b94fc5128f10ec4f4c7f0
Leo Girard <leo.girard@epitech.eu>
2026-05-23 20:47:32 +0200

    fix : replacing links with cp

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@
 read -p "Do you want to install GPU drivers now? (y/n): " confirm
 if [[ "$confirm" == [yY] || "$confirm" == [yY][eE][sS] ]]; then
     
-    yay -S linux-headers nvidia-580xx-utils nvidia-580xx-dkms
+    yay -S --noconfirm linux-headers nvidia-580xx-utils nvidia-580xx-dkms
 else
     echo "Skipping drivers."
 fi
@@ -36,24 +36,22 @@ for config in "${configs[@]}"; do
     if [ -d "$DOTFILES_DIR/$config" ]; then
         TARGET="$HOME/.config/$config"
 
-        # Remove target whether it's a file, folder, or symlink
         rm -rf "$TARGET"
 
-        # Use ln without -f since we just cleared the path
-        ln -s "$DOTFILES_DIR/$config" "$TARGET"
-        echo "Linked $config to ~/.config/"
+        cp -r"$DOTFILES_DIR/$config" "$TARGET"
+        echo "copied $config to ~/.config/"
     fi
 done
 
 echo "--- Linking Assets ---"
 # Link Icons
 if [ -d "$DOTFILES_DIR/icons" ]; then
-    ln -sfn "$DOTFILES_DIR/icons" "$HOME/.local/share/icons/my-icons"
+    cp -r  "$DOTFILES_DIR/icons" "$HOME/.local/share/icons/my-icons"
     echo "Linked icons"
 fi
 
 if [ -d "$DOTFILES_DIR/Wallpapers" ]; then
-    ln -sfn "$DOTFILES_DIR/Wallpapers" "$HOME/Downloads/Wallpapers"
+    cp -r  "$DOTFILES_DIR/Wallpapers" "$HOME/Downloads/Wallpapers"
     echo "Linked Wallpapers"
 fi
 
@@ -70,7 +68,7 @@ home_files=(".zshrc" ".profile" ".zprofile")
 
 for file in "${home_files[@]}"; do
     if [ -f "$DOTFILES_DIR/$file" ]; then
-        ln -sfn "$DOTFILES_DIR/$file" "$HOME/$file"
+        cp  "$DOTFILES_DIR/$file" "$HOME/$file"
         echo "Linked $file to $HOME/"
     else
         echo "Notice: $file not found in $DOTFILES_DIR, skipping."
